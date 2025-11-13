1ab0eb38d4f7945fdf241abd942337a0484640b7
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2025-11-13 23:05:18 +0900

    autoconfig

---
+++ otel-autoconfig-sandbox/README.md
@@ -0,0 +1,9 @@
+# otel-autoconfig-sandbox
+
+
+
+このあたり
+
+- https://opentelemetry.io/docs/specs/otel/configuration/sdk-environment-variables/
+- https://opentelemetry.io/docs/languages/java/configuration/#zero-code-sdk-autoconfigure
+
---
+++ otel-autoconfig-sandbox/src/main/java/epsilongtmyon/sandbox01/Sandbox01Main.java
@@ -0,0 +1,63 @@
+package epsilongtmyon.sandbox01;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import io.opentelemetry.api.metrics.DoubleGauge;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+
+/*
+ * メトリクスの設定
+ */
+public class Sandbox01Main {
+
+	public static void main(String[] args) throws Exception {
+
+		var main = new Sandbox01Main();
+		main.setup();
+
+		OpenTelemetrySdk openTelemetrySdk = AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
+		main.start1(openTelemetrySdk);
+
+		//-----------------------------
+		System.out.print("wait..");
+		System.in.read();
+
+	}
+
+	// システムプロパティ、環境変数が対応しているが
+	// IDE上で設定するのが面倒なので直接設定
+	// システムプロパティが優先度高そう
+	private void setup() {
+
+		Map<String, String> props = new HashMap<>();
+
+		props.put("otel.service.name", "my-service");
+		props.put("otel.resource.attributes", "myKey=001");
+
+		props.put("otel.metric.export.interval", "10000");// default:1min
+
+		props.put("otel.metrics.exporter", "otlp"); //default:otlp
+
+		props.entrySet().forEach(en -> System.setProperty(en.getKey(), en.getValue()));
+	}
+
+	private void start1(OpenTelemetrySdk openTelemetry) throws IOException, InterruptedException {
+		Meter meter1 = openTelemetry.getMeter("my-meter1");
+
+		DoubleGauge m1g1 = meter1.gaugeBuilder("my-meter1-g1").build();
+
+		for (int i = 0; i < 5; i++) {
+
+			m1g1.set(1.5 * i);
+
+			System.out.println("wait " + i);
+			TimeUnit.SECONDS.sleep(5);
+		}
+
+	}
+}
